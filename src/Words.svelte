84e628a66b060119bfe3f8e4a4577ddc35f99499
Nellie <fabybacenelson@gmail.com>
2020-06-09 23:34:06 -0700

    switched to svg

--- src/Words.svelte
+++ src/Words.svelte
@@ -12,8 +12,9 @@
         clientY = mouseEvent.clientY;
         clientX = mouseEvent.clientX;
 
-        e.target.style.top = clientY - e.target.offsetHeight/2 + "px";
-        e.target.style.left = clientX - e.target.offsetWidth/2 + "px";
+        // e.target.offsetHeight/2 
+        e.target.style.top = clientY + "px";
+        e.target.style.left = clientX + "px";
     }
         document.addEventListener("mousemove", listener);
         e.target.style.zIndex = top;
@@ -25,44 +26,60 @@
         document.removeEventListener("mousemove", listener);
     }
 
-    function generateStyle(){
-        let size = (2 + Math.random()*1.1).toFixed(2) + "rem";
+    function generateTextStyle(){
+        let size = (1.7 + Math.random()*1.2).toFixed(2) + "rem";
         let colors = ["#333", "#271d1d", "#1d1d27", "#464646", "#0e0e0a", "#8a1503", "#2a3980", "#282894"];
-        let fonts = ["serif", "sans-serif", "helvetica", "arial", "Times New Roman"];
+        let fonts = ["serif", "sans-serif", "helvetica", "arial", "Times New Roman", "Georgia", "Book Antiqua", "Impact", "Lucida Sans Unicode", "Courier New", "Lucida Console", "Trebuchet MS"];
+        let padding = `${Math.random().toFixed(2)}rem ${Math.random().toFixed(2)}rem ${Math.random().toFixed(2)}rem ${Math.random().toFixed(2)}rem`;
+        let background = `hsl(44, 100%, ${95+Math.floor(Math.random()*6)}%)`;
 
         return `font-size: ${size};
-                color: ${colors[Math.floor(Math.random()*colors.length)]};
-                font-family: ${fonts[Math.floor(Math.random()*fonts.length)]}`;
+                fill: ${colors[Math.floor(Math.random()*colors.length)]};
+                font-family: ${fonts[Math.floor(Math.random()*fonts.length)]};`;
     }
 
+    function generatePaperStyle(){
 
+    }
+
+    function getSVGWidth(svh){
+        console.log(svh)
+    }
 </script>
 
 {#each words as word}
-    <div 
-        on:mousedown={handleDrag}
-        on:mouseup={handleRelease}
-        style={generateStyle()}
-    >
+<svg 
+    width={word.length + 5 + "em"}
+    height="10ex"
+    on:mousedown={handleDrag}
+    on:mouseup={handleRelease}>
+<text 
+    y="50"
+    style={generateTextStyle()}>
         {word}    
-    </div>
+    </text>
+</svg>
+    
 {/each}
 
+
 <style>
-    div{
-        /* padding: 1% 2%; */
-        width: fit-content;
+    svg{
+        /* width: fit-content; */
         background-color: white;
         box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.25);
         position: absolute;
         user-select: none;
     }
+    text{
+        pointer-events: none;
+    }
 
-    div:hover{
+    svg:hover{
         cursor: grab;
     }
 
-    div:active{
+    svg:active{
         cursor: grabbing;
     }
 </style>
