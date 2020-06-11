31a5d563c17f4cb35a4ba13b98bd359f560f799c
Nellie <fabybacenelson@gmail.com>
2020-06-11 15:04:25 -0700

    clip path and drop-shadows added

--- src/App.svelte
+++ src/App.svelte
@@ -4,8 +4,20 @@
 </script>
 
 <main>
+	<div class="pasteHere">
+	
+	</div>
 	<Words {words}/>
 </main>
 
 <style>
+	.pasteHere{
+		width: 8.5in;
+		height: 11in;
+		background-color: whitesmoke;
+		position: absolute;
+		left: calc((100vw - 8.5in) / 2);
+		top: -15%;
+		box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.25);
+	}
 </style>
--- src/Words.svelte
+++ src/Words.svelte
@@ -32,16 +32,12 @@
         let size = (1.7 + Math.random()*1.2).toFixed(2) + "rem";
         let colors = ["#333", "#271d1d", "#1d1d27", "#464646", "#0e0e0a", "#8a1503", "#2a3980", "#282894"];
         let fonts = ["serif", "sans-serif", "helvetica", "arial", "Times New Roman", "Georgia", "Book Antiqua", "Impact", "Lucida Sans Unicode", "Courier New", "Lucida Console", "Trebuchet MS"];
-        let padding = `${Math.random().toFixed(2)}rem ${Math.random().toFixed(2)}rem ${Math.random().toFixed(2)}rem ${Math.random().toFixed(2)}rem`;
-        let background = `hsl(44, 100%, ${95+Math.floor(Math.random()*6)}%)`;
+        // let padding = `${Math.random().toFixed(2)}rem ${Math.random().toFixed(2)}rem ${Math.random().toFixed(2)}rem ${Math.random().toFixed(2)}rem`;
+        // let background = `hsl(44, 100%, ${95+Math.floor(Math.random()*6)}%)`;
 
         return `font-size: ${size};
                 fill: ${colors[Math.floor(Math.random()*colors.length)]};
                 font-family: ${fonts[Math.floor(Math.random()*fonts.length)]};`;
-    }
-
-    function generatePaperStyle(){
-
     }
 
     // function getSVGWidth(svh){
@@ -66,8 +62,8 @@
             svgs[i].setAttribute("height", calcHeight.toFixed(2));
             svgs[i].setAttribute("width", calcWidth.toFixed(2));
 
-            let x = Math.random()*5;
-            let y = Math.random()*10;
+            let x = Math.random()*3;
+            let y = Math.random()*8;
 
             let x1 = Math.random()*20;
             let y1 = y - Math.random()*y;
@@ -75,19 +71,33 @@
             let x2 = Math.random()*(calcWidth - x1);
             let y2 = y - Math.random()*y;
 
-            svgs[i].firstChild.setAttribute("d", 
-            `M ${x} ${y} 
+            let cutTop = `M ${x} ${y} 
              C ${x1.toFixed(2)} ${y1.toFixed(2)}, 
                ${x2.toFixed(2)} ${y2.toFixed(2)}, 
                ${calcWidth.toFixed(2)} 0 
              L ${calcWidth.toFixed(2)} ${calcHeight.toFixed(2)} 
-             L 0 ${calcHeight.toFixed(2)} 
-             Z`);
+             L 0 ${calcHeight.toFixed(2)}  
+             Z`;
+
+            let cutBottom = `M ${x} ${y} 
+             L ${calcWidth.toFixed(2)} 0
+             L ${calcWidth.toFixed(2)} ${calcHeight.toFixed(2)} 
+             C ${(calcWidth - x2).toFixed(2)} ${(calcHeight - y2).toFixed(2)}, 
+               ${(calcWidth - x1).toFixed(2)} ${(calcHeight - y1).toFixed(2)}, 
+               0 ${calcHeight.toFixed(2)} 
+             Z`
+
+            svgs[i].firstChild.setAttribute("d",[cutTop, cutBottom][Math.floor(Math.random()*2)]);
+
+            //adding a clip path that hopefully cuts off text that flows outside of the paper, hard to test though
+            let paper = svgs[i].firstChild;
+            let paperPath = paper.cloneNode(true);
+            svgs[i].querySelector(":nth-child(2)").append(paperPath);
         }
     })
 </script>
 
-{#each words as word}
+{#each words as word, i}
 <svg 
     on:mousedown={handleDrag}
     on:mouseup={handleRelease}
@@ -97,10 +107,15 @@
     >    
     </path>
 
+    <clipPath id={`paper${i}`}>
+    </clipPath>
+    
     <text 
         x={(5 + Math.random()*20).toFixed(2)}
         y={(35 + Math.random()*20).toFixed(2)}
-        style={generateTextStyle()}>
+        style={generateTextStyle()}
+        clip-path={`url(#paper${i})`}
+    >
             {word}    
         </text>
 
@@ -111,9 +126,9 @@
 
 <style>
     svg{
-        /* width: fit-content; */
         position: absolute;
         user-select: none;
+        filter: drop-shadow(2px 2px 2px #2b2b2173);
     }
 
     path{
